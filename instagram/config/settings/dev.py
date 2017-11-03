907d8029f67f8cd6e40aef3f2974d8b6f52e26f9
Che1 <nachwon@gmail.com>
2017-11-03 11:54:34 +0900

    separate dockerfile

--- instagram/config/settings/dev.py
+++ instagram/config/settings/dev.py
@@ -1,3 +1,7 @@
+import random
+import string
+from pprint import pprint
+
 from .base import *
 
 config_secret = json.loads(open(CONFIG_SECRET_DEV_FILE).read())
@@ -19,3 +23,5 @@ STATICFILES_STORAGE = 'config.storages.StaticStorage'
 
 # Databases
 DATABASES = config_secret['django']['databases']
+
+SECRET_KEY = ''.join([random.choice(string.ascii_lowercase) for i in range(40)])
