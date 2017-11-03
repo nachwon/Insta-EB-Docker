def0a2f923207584c4bbaea8f0a47b909beabfdc
Che1 <nachwon@gmail.com>
2017-11-03 14:06:49 +0900

    deploy ready

--- instagram/config/settings/dev.py
+++ instagram/config/settings/dev.py
@@ -1,15 +1,14 @@
 import random
 import string
-from pprint import pprint
 
 from .base import *
 
 config_secret = json.loads(open(CONFIG_SECRET_DEV_FILE).read())
 
 # AWS
-AWS_ACCESS_KEY_ID = config_secret_common['aws']['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = config_secret_common['aws']['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = config_secret_common['aws']['AWS_STORAGE_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = config_secret['aws']['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = config_secret['aws']['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = config_secret['aws']['AWS_STORAGE_BUCKET_NAME']
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_REGION_NAME = 'ap-northeast-2'
 
